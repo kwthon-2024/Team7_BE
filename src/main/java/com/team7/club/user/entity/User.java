01b1ee08d2e25d653497e09ef4b6e74fe0a48513
Eom-Ye-rim <dpfladlwkdrn@naver.com>
2024-11-16 00:41:10 +0900

    [feat] User Entity 생성

---
+++ src/main/java/com/team7/club/user/entity/User.java
@@ -0,0 +1,85 @@
+package com.team7.club.user.entity;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.*;
+
+import java.util.Collection;
+import java.util.List;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@Entity
+@Table(name="users")
+public class User extends BaseTime implements UserDetails {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column
+	private String email;
+
+	@Column
+	private String name;
+
+	@Column
+	private String password;
+
+	@Column
+	private String studentNumber;
+
+	@Column
+	private String clubName;
+
+
+	@Enumerated(EnumType.STRING)
+	@Column(name = "club_role", nullable = false)
+	private ClubRole clubRole;
+
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		// clubRole에 기반한 권한 부여
+		return List.of(new SimpleGrantedAuthority(clubRole.name()));
+	}
+
+
+	@Override
+	public String getUsername() {
+		return email;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
+}
